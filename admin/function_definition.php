befcd36679e43d76cba5f1bb08fc8059351b54b9
Jannai Heng Yiren <d1360588@ncsdevnet.com>
2024-04-08 21:41:54 +0800

    Add admin retrieveOrders and retrieveStallDetails, also added updateDBOrderSus

--- admin/function_definition.php
+++ admin/function_definition.php
@@ -35,27 +35,58 @@ include '../common/initialize_all.php';
             'remark' => ''
         );
             // filter items through stall_id
-            $query = "SELECT DISTINCT * FROM
-            (SELECT order_items.order_id, orders.order_datetime, stalls.stall_id, food_details_table.food_details, order_items.collection_mode,  order_items.order_status, Account_information.firstname, Account_information.lastname, delivery_address.address, Account_information.e_mail, Account_information.mobile_no, order_items.remark
-                    FROM order_items
-                    
-                    INNER JOIN
-                    
-                    (SELECT order_id, GROUP_CONCAT( item_details SEPARATOR '<br/><br/>' ) AS food_details FROM (
-                        SELECT * , CONCAT( item_quantity, 'x ', (SELECT dish_name FROM dishes WHERE order_items.dish_id = dishes.dish_id), '<br/>', item_pref) AS item_details FROM order_items WHERE stall_id = " . $_SESSION['stall_id'] . ")subquery
-                        GROUP BY subquery.order_id
-                    ) food_details_table
-                    
-                    ON order_items.order_id = food_details_table.order_id
-                    
-                    INNER JOIN orders ON orders.order_id = order_items.order_id
-                    INNER JOIN Account_information ON Account_information.account_id = order_items.account_id
-                    INNER JOIN delivery_address ON delivery_address.order_id = order_items.order_id
-                    INNER JOIN stalls ON stalls.stall_id = order_items.stall_id) subquery2
-            
-                    WHERE subquery2.stall_id = " . $_SESSION['stall_id'] . "
-            
-                    ORDER BY subquery2.order_id DESC;";
+            $query = "SELECT
+            u.email, u.contact_no, c.first_name, c.last_name, q3.datetime_ordered, q3.order_cost, q3.collection_mode, q3.order_id, q3.order_status, q3.food_details, q3.stall_comment, da.address
+            FROM
+            ( -- food details
+                SELECT q2.customer_id, q2.datetime_ordered, q2.order_cost, q2.collection_mode, q2.order_id, q2.order_status, GROUP_CONCAT(item_details SEPARATOR '<br/>') food_details, q2.stall_comment
+                FROM
+                (
+                    SELECT q1.customer_id, q1.datetime_ordered, q1.order_cost, q1.collection_mode, q1.order_id, q1.order_status, CONCAT(q1.quantity, 'x ', q1.food_name, '<br/>', q1.food_details, '<br/>-', q1.special_instruction, '<br/>') as item_details, q1.stall_comment
+                    FROM
+                    (
+                        SELECT stall_food_id_query.customer_id, stall_food_id_query.datetime_ordered, stall_food_id_query.order_cost, stall_food_id_query.collection_mode, f_out.food_name, stall_food_id_query.order_id, stall_food_id_query.order_item_id, stall_food_id_query.quantity, stall_food_id_query.order_status, 
+                        GROUP_CONCAT(CONCAT('-', ip.food_preference) SEPARATOR '<br/>') food_details, stall_food_id_query.special_instruction, stall_food_id_query.stall_comment
+                        FROM food f_out
+    
+                        INNER JOIN 
+                        (SELECT
+                        o.customer_id,
+                        o.datetime_ordered,
+                        o.order_cost,
+                        o.collection_mode,
+                        oi.stall_id,
+                        oi.food_id,
+                        oi.order_item_id,
+                        oi.quantity,
+                        oi.order_status,
+                        oi.order_id,
+                        oi.special_instruction,
+                        oi.stall_comment
+                        FROM `order` o
+                        INNER JOIN order_item oi
+                        ON o.order_id = oi.order_id
+                        INNER JOIN stall s
+                        ON oi.stall_id = s.stall_id
+                        AND oi.stall_id = " . $_SESSION['stall_id'] . "
+                        INNER JOIN food f
+                        ON oi.food_id = f.food_id
+                        GROUP BY oi.food_id, oi.stall_id) stall_food_id_query
+                        ON f_out.stall_id = stall_food_id_query.stall_id
+                        AND f_out.food_id = stall_food_id_query.food_id
+    
+                        INNER JOIN item_preference ip
+                        on ip.order_item_id = stall_food_id_query.order_item_id
+                        GROUP BY stall_food_id_query.order_item_id, stall_food_id_query.order_status) q1
+                    ) q2
+                    GROUP BY q2.order_id
+                ) q3
+            INNER JOIN customer c
+            ON q3.customer_id = c.c_user_id 
+            INNER JOIN `user` u
+            ON c.c_user_id = u.user_id
+            LEFT JOIN delivery_address da
+            ON q3.order_id = da.order_id;";
             
             $result = $db->query($query);
 
@@ -63,16 +94,16 @@ include '../common/initialize_all.php';
                 if ($result->num_rows > 0) {
                     while($row = $result->fetch_assoc()) {
                     $order_adding['order_id'] = $row['order_id'];
-                    $order_adding['order_datetime'] = $row['order_datetime'];
+                    $order_adding['order_datetime'] = $row['datetime_ordered'];
                     $order_adding['food_details'] = $row['food_details'];
                     $order_adding['collection_mode'] = $row['collection_mode'];
                     $order_adding['order_status'] = $row['order_status'];
-                    $order_adding['firstname'] = $row['firstname'];
-                    $order_adding['lastname'] = $row['lastname'];
+                    $order_adding['firstname'] = $row['first_name'];
+                    $order_adding['lastname'] = $row['last_name'];
                     $order_adding['address'] = $row['address'];
-                    $order_adding['e_mail'] = $row['e_mail'];
-                    $order_adding['mobile_no'] = $row['mobile_no'];
-                    $order_adding['remark'] = $row['remark'];
+                    $order_adding['e_mail'] = $row['email'];
+                    $order_adding['mobile_no'] = $row['contact_no'];
+                    $order_adding['remark'] = $row['stall_comment'];
                         
                         array_push($order, $order_adding);
                     }
@@ -83,6 +114,8 @@ include '../common/initialize_all.php';
                 echo $db->error;
             }
     }
+
+    // " . $_SESSION['stall_id'] . "
 
     function retrieveOrderByOrderID($order_id) {
         include '../common/connectDB.php';
@@ -221,14 +254,15 @@ include '../common/initialize_all.php';
         include '../common/initialize_all.php';
         include '../common/connectDB.php';
 
-        $query = "UPDATE order_items
-        SET order_status = '" . $updated_status ."', remark = '" . $updated_remark . "'
+        $query = "UPDATE order_item
+        SET order_status = '" . $updated_status ."', stall_comment = '" . $updated_remark . "'
         WHERE order_id = " . $order_id . " AND stall_id = " . $stall_id . ";";
 
         $result = $db->query($query);
 
         if ($result) {
-            return emailCustomerOrderStatus($order_id); //return false if there is error in the function
+            return true;
+            // return emailCustomerOrderStatus($order_id); //return false if there is error in the function
         } else {
             echo 'Error: ' . $db->error;
             return false;
@@ -255,7 +289,12 @@ include '../common/initialize_all.php';
             'open_status' => ''
         );
 
-        $query = "SELECT * FROM stalls WHERE stall_id = $stall_id";
+        $query = "SELECT u.contact_no , s.* FROM stall s
+        INNER JOIN admin a
+        ON a.stall_id = s.stall_id
+        INNER JOIN `user` u
+        ON a.s_user_id = u.user_id
+        WHERE s.stall_id = $stall_id";
 
         $result = $db->query($query);
 
@@ -266,7 +305,8 @@ include '../common/initialize_all.php';
                 $stall_details['stall_name'] = $row['stall_name'];
                 $stall_details['announcement'] = $row['announcement'];
                 $stall_details['description'] = $row['description'];
-                $stall_details['opening_hour'] = $row['opening_hour'];
+                $stall_details['opening_hour_start'] = (new DateTime($row['opening_hour_start']))->format("h:i:s");
+                $stall_details['opening_hour_end'] = (new DateTime($row['opening_hour_end']))->format("h:i:s");
                 $stall_details['contact_no'] = $row['contact_no'];
                 $stall_details['unit_no'] = $row['unit_no'];
                 $stall_details['open_status'] = $row['open_status'];
